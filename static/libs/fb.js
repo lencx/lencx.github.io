369e7602d00fe97b979c78d6b8aa3b38551cd5bc
Len <cxin1314@gmail.com>
2017-03-21 02:00:20 +0800

    about

---
+++ static/libs/fb.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBv04C5NEHihi2c8q42w00ytm969Rq_2Dk",
+    authDomain: "lencx-d2b43.firebaseapp.com",
+    databaseURL: "https://lencx-d2b43.firebaseio.com",
+    storageBucket: "lencx-d2b43.appspot.com",
+    messagingSenderId: "556431075239"
+};
+firebase.initializeApp(config);
